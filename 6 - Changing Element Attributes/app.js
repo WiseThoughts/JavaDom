bb78d1028858b4a74c3e9dbda18b71e48de24a46
Benny Nebreda <benny.nebreda@gmail.com>
2022-05-23 11:45:37 +0100

    end of day one

--- 6 - Changing Element Attributes/app.js
+++ 6 - Changing Element Attributes/app.js
@@ -1,1 +1,8 @@
+const image = document.getElementById("image");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+console.log(image);
 
+submit.addEventListener ("click", ()=>{
+    image.src = input.value;
+});
--- 7 - Styling Elements with JS/app.js
+++ 7 - Styling Elements with JS/app.js
@@ -1,1 +1,13 @@
+const image = document.getElementById("cat");
+const hide = document.getElementById("submit")
 
+console.log(image.style)
+hide.addEventListener("click", ()=>{
+    if (image.style.display == "none"){
+        image.style.display = "block";
+        hide.textContent = "Hide";
+    }else{
+        image.style.display = "none";
+        hide.textContent = "Show";
+    }
+})
